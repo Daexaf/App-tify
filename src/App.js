8ff82a92613648974c08a7980cb7316c1b89cffb
Daexaf <ihsan64.mi@gmail.com>
2022-03-23 10:40:05 +0700

    Adding data from API data.js and call it in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Create Playlist</h1>
+
+      <div class="deskripsi">
+        <div class="logo">
+          <img src={data.album.images[1].url} alt="" />
+        </div>
+        <div class="desc-content">
+          <h3>Title: {data.name}</h3>
+          <p>Artist: {data.album.artists[0].name}</p>
+          <p>Albums: {data.album.name}</p>
+          <button type="submit" id="play">
+            Select
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
